c2248496a36a0a5d51adee604b7bba61f00a8ba4
unknown <you@example.com>
2026-02-01 15:57:31 +0200

    Nameplate fix

--- ShestakUI/Modules/UnitFrames/Nameplates.lua
+++ ShestakUI/Modules/UnitFrames/Nameplates.lua
@@ -56,11 +56,8 @@ function frame:PLAYER_LOGIN()
 	if C.nameplate.only_name then
 		SetCVar("nameplateShowOnlyNameForFriendlyPlayerUnits", 1)
 	end
-<<<<<<< HEAD
+
 	SetCVar("nameplateShowOnlyNames", 1)
-=======
-	-- SetCVar("nameplateShowOnlyNames", 0)
->>>>>>> main
 
 	local function changeFont(self, size)
 		local mult = size or 1
