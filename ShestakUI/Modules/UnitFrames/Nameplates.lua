74fff02f6efc4b0f464650c7e1ecfcbdf6ec9b56
unknown <you@example.com>
2026-03-31 13:05:29 +0300

    [Nameplate] add color interrupter...Itrix

--- ShestakUI/Modules/UnitFrames/Nameplates.lua
+++ ShestakUI/Modules/UnitFrames/Nameplates.lua
@@ -474,9 +474,16 @@ end
 local function CastInterrupted(self, unit, interruptedBy)
 	self:GetStatusBarTexture():SetVertexColor(0.2, 0.2, 0.2)
 	if interruptedBy then
-		local unitName = UnitNameFromGUID(interruptedBy)
+		local _, class, _, _, _, unitName = GetPlayerInfoByGUID(interruptedBy)
+		local coloredName = unitName
+		if class then
+			classColor = C_ClassColor.GetClassColor(class)
+			if classColor then
+				coloredName = classColor:WrapTextInColorCode(unitName)
+			end
+		end	
 		if unitName then
-			self.Text:SetFormattedText("%s [%s]", INTERRUPTED, unitName)
+			self.Text:SetText(INTERRUPTED .. " [" .. coloredName .. "]")
 		end
 	end
 end
